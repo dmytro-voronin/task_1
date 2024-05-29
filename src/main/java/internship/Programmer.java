68a9a40adc84c206f97a60af43e8a39e25bfa155
dmytro-voronin <d.voronin.owen@gmail.com>
2024-05-29 14:47:33 +0300

    add Programmer class

---
+++ src/main/java/internship/Programmer.java
@@ -0,0 +1,13 @@
+package internship;
+
+public class Programmer extends Employee {
+
+    public Programmer(String firstName, String lastName, int id) {
+        super(firstName, lastName, id);
+    }
+
+    @Override
+    public double getMonthlySalary() {
+        return getSalaryRate() * (getWorkedHours() / 160.0);
+    }
+}
