b1d441f18d606ce967b6ea877dc49f547721edd6
dmytro-voronin <d.voronin.owen@gmail.com>
2024-05-29 14:49:18 +0300

    add Manager class

---
+++ src/main/java/internship/Manager.java
@@ -0,0 +1,18 @@
+package internship;
+
+public class Manager extends Employee {
+
+    public Manager(String firstName, String lastName, int id) {
+        super(firstName, lastName, id);
+    }
+
+    @Override
+    public double getMonthlySalary() {
+        double workedPercentage = getWorkedTimePercentage();
+        if (workedPercentage >= 100) {
+            return getSalaryRate();
+        } else {
+            return getSalaryRate() * (workedPercentage / 100);
+        }
+    }
+}
